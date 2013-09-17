6b5bc3b05e09e7142b27d11648ff3a34d3cb6fe0
code-vortex <darkbrotherhood.assassin@gmail.com>
2013-09-17 07:08:34 +0200

    modified migration files

--- db/migrate/20130904073940_addremovecolumntomodel.rb
+++ db/migrate/20130904073940_addremovecolumntomodel.rb
@@ -1,6 +1,6 @@
 class Addremovecolumntomodel < ActiveRecord::Migration
   def up
-  	remove_column :topics, :ticket_id, :integer
+  	#remove_column :topics, :ticket_id, :integer
   	#add_column :tickets, :topic_id, :integer
   end
 
