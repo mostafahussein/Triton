f0483c5ad182a3ca026a3c3f9773398006ec1674
code-vortex <darkbrotherhood.assassin@gmail.com>
2013-09-17 07:38:19 +0200

    modified migration files

--- db/migrate/20130907174307_changedatatype.rb
+++ db/migrate/20130907174307_changedatatype.rb
@@ -1,9 +1,9 @@
 class Changedatatype < ActiveRecord::Migration
   def up
-  	change_column :ticket_comments, :ticket_id, :integer
-  	change_column :ticket_comments, :user_id, :integer
-  	change_column :ticket_comments, :state_id, :integer
-  	change_column :ticket_comments, :previous_state_id, :integer
+  	#change_column :ticket_comments, :ticket_id, :integer
+  	#change_column :ticket_comments, :user_id, :integer
+  	#change_column :ticket_comments, :state_id, :integer
+  	#change_column :ticket_comments, :previous_state_id, :integer
   end
 
   def down
