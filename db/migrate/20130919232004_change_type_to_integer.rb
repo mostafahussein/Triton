48cbc910eab6192e354ce772271cab3fc57b93d2
code-vortex <darkbrotherhood.assassin@gmail.com>
2013-09-20 03:25:54 +0200

    seeds file modified

--- db/migrate/20130919232004_change_type_to_integer.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTypeToInteger < ActiveRecord::Migration
-  def up
-  	change_column :messages, :sender_id, :integer
-  	change_column :messages, :recepient_id, :integer
-  end
-
-  def down
-  end
-end
